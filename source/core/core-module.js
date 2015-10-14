1d55b1f7904d8d1153f7ab070381299305d8aef1
杨佳军 <651380735@qq.com>
2015-10-14 16:05:33 +0800

    no message

--- source/core/core-module.js
+++ source/core/core-module.js
@@ -27,18 +27,20 @@ var CoreModule = angular.module('myApp',[
     .controller('myAppCtrl',['$rootScope',function($rootScope){
         $rootScope.is_weixn = is_weixn;
 
-        //页面加载完成后自适应屏幕
-        $rootScope.$on('$viewContentLoaded', function() {
-            $rootScope.winheight= $(window).height();
-            $rootScope.winwidth = $(window).width();
-            $rootScope.header = is_weixn()?0:56;
-            var el = $("#message_box");
-            if(el.length == 1){
-                $(el[0]).css('max-height',$rootScope.winheight-$rootScope.header-48);
-                $("#message_input").css('width',$rootScope.winwidth-48-48-52-30);
-                $("#audio_input").css('width',$rootScope.winwidth-48-52-10);
-            }
-        });
+        if(is_weixn()){
+            //页面加载完成后自适应屏幕
+            $rootScope.$on('$viewContentLoaded', function() {
+                $rootScope.winheight= $(window).height();
+                $rootScope.winwidth = $(window).width();
+                $rootScope.header = is_weixn()?0:56;
+                var el = $("#message_box");
+                if(el.length == 1){
+                    $(el[0]).css('max-height',$rootScope.winheight-$rootScope.header-48);
+                    $("#message_input").css('width',$rootScope.winwidth-48-48-52-30);
+                    $("#audio_input").css('width',$rootScope.winwidth-48-52-10);
+                }
+            });
+        }
 
         /**
          * 判断是否在微信打开
@@ -52,6 +54,6 @@ var CoreModule = angular.module('myApp',[
                 return false;
             }
         }
-    }])
+    }]);
 
 export default CoreModule
