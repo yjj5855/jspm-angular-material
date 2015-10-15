324f6a97bfad460dd50900ae4c60676432881eda
杨佳军 <651380735@qq.com>
2015-10-15 14:31:32 +0800

    bug

--- source/core/core-module.js
+++ source/core/core-module.js
@@ -27,24 +27,27 @@ var CoreModule = angular.module('myApp',[
     ChatModule.name,
 ])
     .config(CoreRouter)
+
+angular.module('myApp')
     .controller('myAppCtrl',['$rootScope',function($rootScope){
-        $rootScope.is_weixn = window.is_weixn();
+    $rootScope.is_weixn = window.is_weixn();
 
-        if($rootScope.is_weixn){
-            //页面加载完成后自适应屏幕
-            $rootScope.$on('$viewContentLoaded', function() {
-                $rootScope.winheight= $(window).height();
-                $rootScope.winwidth = $(window).width();
-                $rootScope.header = $rootScope.is_weixn?0:56;
-                var el = $("#message_box");
-                if(el.length == 1){
-                    $(el[0]).css('max-height',$rootScope.winheight-$rootScope.header-48);
-                    $("#message_input").css('width',$rootScope.winwidth-48-48-52-30);
-                    $("#audio_input").css('width',$rootScope.winwidth-48-52-10);
-                }
-            });
-        }
+    if($rootScope.is_weixn){
+        //页面加载完成后自适应屏幕
+        $rootScope.$on('$viewContentLoaded', function() {
+            $rootScope.winheight= $(window).height();
+            $rootScope.winwidth = $(window).width();
+            $rootScope.header = $rootScope.is_weixn?0:56;
+            var el = $("#message_box");
+            if(el.length == 1){
+                $(el[0]).css('max-height',$rootScope.winheight-$rootScope.header-48);
+                $("#message_input").css('width',$rootScope.winwidth-48-48-52-30);
+                $("#audio_input").css('width',$rootScope.winwidth-48-52-10);
+            }
+        });
+    }
 
-    }]);
+}]);
+
 
 export default CoreModule
